0bc9fdbb2e58162a9745d2f2698b98be0125c7c9
RosarioB <borgesiros@gmail.com>
2023-05-14 08:10:39 +0200

    minor changes

--- sql-scripts/employee-directory.sql
+++ sql-scripts/employee-directory.sql
@@ -20,9 +20,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	(1,'Leslie','Andrews','leslie@someemail.com'),
+	(2,'Emma','Baumgarten','emma@someemail.com'),
+	(3,'Avani','Gupta','avani@someemail.com'),
+	(4,'Yuri','Petrov','yuri@someemail.com'),
+	(5,'Juan','Vega','juan@someemail.com');
 
