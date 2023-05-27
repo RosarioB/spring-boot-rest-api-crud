a33c9e5ada2cf733b8b1d0b080d3611e16d707a8
RosarioB <borgesiros@gmail.com>
2023-05-27 12:32:17 +0200

    minor changes

--- src/main/java/com/rosariob/springboot/cruddemo/entity/Employee.java
+++ src/main/java/com/rosariob/springboot/cruddemo/entity/Employee.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Table;
 @Table(name = "employee")
 public class Employee {
 
-    // define fields
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -23,7 +22,6 @@ public class Employee {
     @Column(name = "email")
     private String email;
 
-    // define constructors
     public Employee(){}
     public Employee(String firstName, String lastName, String email){
         this.firstName = firstName;
@@ -31,7 +29,6 @@ public class Employee {
         this.email = email;
     }
 
-    // define getters / setters
     public int getId() {
         return id;
     }
@@ -64,7 +61,6 @@ public class Employee {
         this.email = email;
     }
 
-    // define toString
     @Override
     public String toString() {
         return "Employee{" +
